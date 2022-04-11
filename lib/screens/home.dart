d0c4a35c0c18010960355f5429b92c243c300002
Christian <christian_nagata@hotmail.com>
2022-04-11 20:40:12 -0300

    crashlytics: botão para crash forçado

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -32,6 +33,16 @@ class Home extends StatelessWidget {
                     context.read<AuthenticationService>().signOut();
                   },
                   child: const Text('Sign out'),
+                  style: Theme.of(context).elevatedButtonTheme.style,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 48),
+                child: ElevatedButton(
+                  onPressed: () {
+                    FirebaseCrashlytics.instance.crash();
+                  },
+                  child: const Text('Crash test'),
                   style: Theme.of(context).elevatedButtonTheme.style,
                 ),
               ),
